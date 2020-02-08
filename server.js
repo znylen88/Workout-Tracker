cfba2f8338c16beb9c21f3bd10caadbf80b2429e
Zachary Nylen <znylen@aol.com>
2020-02-07 17:01:41 -0800

    Update

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 require("./routes/api.js")(app);
 require("./routes/view.js")(app);
